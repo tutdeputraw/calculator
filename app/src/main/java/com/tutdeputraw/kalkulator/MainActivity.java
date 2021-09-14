7eda0b3288a047696230de9502ccaee9e077f5f8
tutdeputraw <anoboy.code@gmail.com>
2021-09-14 13:21:00 +0700

    add the calculator functionality

--- app/src/main/java/com/tutdeputraw/kalkulator/MainActivity.java
+++ app/src/main/java/com/tutdeputraw/kalkulator/MainActivity.java
@@ -3,126 +3,190 @@ package com.tutdeputraw.kalkulator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 public class MainActivity extends AppCompatActivity {
-    private TextView formula;
-    private TextView result;
-    private ArrayList<String> list = new ArrayList<>();
+    private TextView workingsTV;
+    private TextView resultTV;
+    private String workings = "";
+    private String formula = "";
+    private String tempFormula = "";
+    private boolean leftBracket = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        formula = findViewById(R.id.tv_number);
-        result = findViewById(R.id.tv_result);
+        initTextViews();
     }
 
-    public void clickCalculate(View view) {
+    private void initTextViews() {
+        workingsTV = findViewById(R.id.tv_number);
+        resultTV = findViewById(R.id.tv_result);
     }
 
-    public void clickAc(View view) {
-        list.clear();
-        convertArrayToString();
+    private void setWorkings(String givenValue) {
+        workings = workings + givenValue;
+        workingsTV.setText(workings);
     }
 
-    public void clickDel(View view) {
-        list.remove(list.size() - 1);
-        convertArrayToString();
+    public void equalsOnClick(View view) {
+        Double result = null;
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("rhino");
+        checkForPowerOf();
+
+        try {
+            result = (double) engine.eval(formula);
+        } catch (ScriptException e) {
+            Toast.makeText(this, "Invalid Input", Toast.LENGTH_SHORT).show();
+        }
+
+        if (result != null)
+            resultTV.setText(String.valueOf(result.doubleValue()));
     }
 
-    public void clickPlusMinus(View view) {
-        list.add("(- ");
-        convertArrayToString();
+    private void checkForPowerOf() {
+        ArrayList<Integer> indexOfPowers = new ArrayList<>();
+        for (int i = 0; i < workings.length(); i++) {
+            if (workings.charAt(i) == '^')
+                indexOfPowers.add(i);
+        }
+
+        formula = workings;
+        tempFormula = workings;
+        for (Integer index : indexOfPowers) {
+            changeFormula(index);
+        }
+        formula = tempFormula;
     }
 
-    public void clickDivision(View view) {
-        list.add(" / ");
-        convertArrayToString();
+    private void changeFormula(Integer index) {
+        String numberLeft = "";
+        String numberRight = "";
+
+        for (int i = index + 1; i < workings.length(); i++) {
+            if (isNumeric(workings.charAt(i)))
+                numberRight = numberRight + workings.charAt(i);
+            else
+                break;
+        }
+
+        for (int i = index - 1; i >= 0; i--) {
+            if (isNumeric(workings.charAt(i)))
+                numberLeft = numberLeft + workings.charAt(i);
+            else
+                break;
+        }
+
+        String original = numberLeft + "^" + numberRight;
+        String changed = "Math.pow(" + numberLeft + "," + numberRight + ")";
+        tempFormula = tempFormula.replace(original, changed);
     }
 
-    public void clickX(View view) {
-        list.add(" * ");
-        convertArrayToString();
+    private boolean isNumeric(char c) {
+        if ((c <= '9' && c >= '0') || c == '.')
+            return true;
+
+        return false;
     }
 
-    public void clickMinus(View view) {
-        list.add(" - ");
-        convertArrayToString();
+    public void clearOnClick(View view) {
+        workingsTV.setText("");
+        workings = "";
+        resultTV.setText("");
+        leftBracket = true;
     }
 
-    public void clickPlus(View view) {
-        list.add(" + ");
-        list.get(list.size()-1);
-        convertArrayToString();
+    public void deleteOnClick(View view) {
+        String newValue = "";
+        for (int i = 0; i < workings.length() - 1; i++) {
+            newValue += workings.charAt(i);
+        }
+        workings = newValue;
+        workingsTV.setText(workings);
     }
 
-    public void clickDot(View view) {
-        list.add(".");
-        convertArrayToString();
+    public void bracketOnClick(View view) {
+        if (leftBracket) {
+            setWorkings("(");
+            leftBracket = false;
+        } else {
+            setWorkings(")");
+            leftBracket = true;
+        }
     }
 
-    public void clickPercent(View view) {
-        list.add(" % ");
-        convertArrayToString();
+    public void divisionOnClick(View view) {
+        setWorkings("/");
     }
 
-    public void click0(View view) {
-        list.add("0");
-        convertArrayToString();
+    public void timesOnClick(View view) {
+        setWorkings("*");
     }
 
-    public void click1(View view) {
-        list.add("1");
-        convertArrayToString();
+    public void minusOnClick(View view) {
+        setWorkings("-");
     }
 
-    public void click2(View view) {
-        list.add("2");
-        convertArrayToString();
+    public void plusOnClick(View view) {
+        setWorkings("+");
     }
 
-    public void click3(View view) {
-        list.add("3");
-        convertArrayToString();
+    public void dotOnClick(View view) {
+        setWorkings(".");
     }
 
-    public void click4(View view) {
-        list.add("4");
-        convertArrayToString();
+    public void percentOnClick(View view) {
+        setWorkings("%");
     }
 
-    public void click5(View view) {
-        list.add("5");
-        convertArrayToString();
+    public void zeroOnClick(View view) {
+        setWorkings("0");
     }
 
-    public void click6(View view) {
-        list.add("6");
-        convertArrayToString();
+    public void oneOnClick(View view) {
+        setWorkings("1");
+        System.out.println("hai");
     }
 
-    public void click7(View view) {
-        list.add("7");
-        convertArrayToString();
+    public void twoOnClick(View view) {
+        setWorkings("2");
     }
 
-    public void click8(View view) {
-        list.add("8");
-        convertArrayToString();
+    public void threeOnClick(View view) {
+        setWorkings("3");
     }
 
-    public void click9(View view) {
-        list.add("9");
-        convertArrayToString();
+    public void fourOnClick(View view) {
+        setWorkings("4");
     }
 
-    private void convertArrayToString() {
-        formula.setText(TextUtils.join("", list));
+    public void fiveOnClick(View view) {
+        setWorkings("5");
+    }
+
+    public void sixOnClick(View view) {
+        setWorkings("6");
+    }
+
+    public void sevenOnClick(View view) {
+        setWorkings("7");
+    }
+
+    public void eightOnClick(View view) {
+        setWorkings("8");
+    }
+
+    public void nineOnClick(View view) {
+        setWorkings("9");
     }
 }
